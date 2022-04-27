66aab44f65fd484c126467a3237ecccafbd160a9
Volodymyr Golub <volodymyr.golub@justanswer.com>
2022-04-27 13:13:14 +0300

    Removed wait.sh

--- Helm-01-Basic templating/foreground.sh
+++ Helm-01-Basic templating/foreground.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+#!/bin/bash
 
-ls -lh /usr/local/bin/
-wait.sh; sleep 10; wait.sh
+show_progress()
+{
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp  
+  echo -n "Configuring"
+  while true; do 
+    sudo grep -i "done" /root/katacoda-background-finished &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Configured"
+}
+
+show_progress
