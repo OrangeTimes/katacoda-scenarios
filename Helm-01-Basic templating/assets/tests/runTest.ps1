e5233b53e8236b3106e15c2a8c6bc915f507bdb2
Volodymyr Golub <volodymyr.golub@justanswer.com>
2022-04-28 17:03:51 +0300

    Updated test configuration

--- Helm-01-Basic templating/assets/tests/runTest.ps1
+++ Helm-01-Basic templating/assets/tests/runTest.ps1
@@ -6,8 +6,7 @@ param(
 Import-Module Pester -PassThru
 $config = [PesterConfiguration]::Default;
 $config.Run.Path = $testLocation;
-$config.Run.PassThru = $true;
-$config.Run.Exit = $true
+$config.Run.EnableExit = $true
 $config.Output.Verbosity = "None";
 
 return Invoke-Pester -Configuration $config ;
