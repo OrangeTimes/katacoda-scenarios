4a94eb1ad93e910d68ef3133e5437c46d6ff92f5
Volodymyr Golub <volodymyr.golub@justanswer.com>
2022-04-28 13:16:05 +0300

    Added powershell

--- Helm-01-Basic templating/background.sh
+++ Helm-01-Basic templating/background.sh
@@ -4,4 +4,10 @@ echo "This is a background script with a long running process"
 
 curl -L https://get.helm.sh/helm-v3.2.2-linux-amd64.tar.gz | tar xvz && mv linux-amd64/helm /usr/local/bin/helm
 
+wget -q https://github.com/PowerShell/PowerShell/releases/download/v7.2.2/powershell-lts_7.2.2-1.deb_amd64.deb
+# Install the downloaded package
+sudo dpkg -i powershell-lts_7.2.2-1.deb_amd64.deb
+# Resolve missing dependencies and finish the install (if necessary)
+sudo apt-get install -f
+pwsh -c Install-Module -Name Pester -Force
 echo "done" >> /opt/.backgroundfinished
