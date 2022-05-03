2284fcd34cdb29ba4e60f688a176b4f3e6862ad0
Volodymyr Golub <volodymyr.golub@justanswer.com>
2022-05-03 13:30:46 +0300

    Added more debug info

--- Helm-01-Basic templating/step1-verify.sh
+++ Helm-01-Basic templating/step1-verify.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pwsh -File ~/.hidden/tests/runTest.ps1 -testLocation '~/.hidden/tests/chart01'
+pwsh -File ~/.hidden/tests/runTest.ps1 -testLocation '~/.hidden/tests/chart01' >> /tmp/result.txt 2>&1
+echo $? >> /tmp/hello.txt 2>&1
