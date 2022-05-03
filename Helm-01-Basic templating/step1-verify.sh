185475f15dd0275d8efaeae01a940cc937c88df2
Volodymyr Golub <volodymyr.golub@justanswer.com>
2022-05-03 15:37:32 +0300

    updated scenario

--- Helm-01-Basic templating/step1-verify.sh
+++ Helm-01-Basic templating/step1-verify.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-pwsh -File ~/.hidden/tests/runTest.ps1 -testLocation '~/.hidden/tests/chart01' 
-if [ $? -eq 0 ]
-then
-   exit 0 && echo "done"
-else
-  echo "The script failed" >> /tmp/result.txt 
-  exit 1
-fi
+pwsh -File ~/.hidden/tests/runTest.ps1 -testLocation '~/.hidden/tests/chart01' && echo "done"
