71041f03edea1d1656dd8f8c852c28067c64aac4
Volodymyr Golub <volodymyr.golub@justanswer.com>
2022-05-03 14:03:30 +0300

    Added step

--- Helm-01-Basic templating/step1-verify.sh
+++ Helm-01-Basic templating/step1-verify.sh
@@ -2,8 +2,7 @@
 pwsh -File ~/.hidden/tests/runTest.ps1 -testLocation '~/.hidden/tests/chart01' 
 if [ $? -eq 0 ]
 then
-  echo "The script ran ok" >> /tmp/result.txt
-  exit 0
+   exit 0 && echo "done"
 else
   echo "The script failed" >> /tmp/result.txt 
   exit 1
