83c4dd979b1d78825fefaa5f2d37006347f6b275
Volodymyr Golub <volodymyr.golub@justanswer.com>
2022-05-04 12:26:50 +0300

    Updated tests

--- Helm-01-Basic templating/assets/tests/chart01/step1.Tests.ps1
+++ Helm-01-Basic templating/assets/tests/chart01/step1.Tests.ps1
@@ -1,15 +1,14 @@
 
 Describe 'Testing Helm Rendering' {
     BeforeAll {
-        $chartPath = Join-Path $PSScriptRoot "../../../charts/chart01" -Resolve
+        $chartPath = (Test-Path "../../../assets") ? (Join-Path $PSScriptRoot "../../../assets/charts/chart01" -Resolve) : (Join-Path $PSScriptRoot "../../../charts/chart01" -Resolve)
     }
-    
-    
+        
     It 'Template rendering should not throw exception' { 
         helm template $chartPath 2>&1 | Out-String | Should -Not -Match "Error" -Because 'No rendering error should be in in helm template'
     }
 
-    It 'Rendered template should match expected file' { 
+    It 'Rendered template should contain values from values.yaml' { 
         $actual =  helm template $chartPath 2>&1 | Out-String
         $actual | Should -Match "name: myhpa" -Because 'Template should contain name'
         $actual | Should -Match "minReplicas: 1" -Because 'minReplicas should be 1'
