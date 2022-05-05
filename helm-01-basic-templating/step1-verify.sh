b2193a18937f4777f707a8153cdcb10791b97bab
Volodymyr Golub <volodymyr.golub@justanswer.com>
2022-05-05 17:30:38 +0300

    Updated verify logic

--- helm-01-basic-templating/step1-verify.sh
+++ helm-01-basic-templating/step1-verify.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-pwsh -File ~/.hidden/tests/runTest.ps1 -testLocation '~/.hidden/tests/step01' && echo "done"
+pwsh -File ~/.hidden/tests/runTest.ps1 -testLocation '~/.hidden/tests/step01';
+if [ $? -eq 0 ] 
+then 
+  echo "exit 0" >> /tmp/result.txt
+  echo "done"
+else 
+  echo "exit 1" >> /tmp/result.txt
+  exit 1
+fi
+
+
 # If the user can proceed the script should output "done" with exit code 0.
 # to debug the test, use: <command to debug> >> /tmp/result.txt 2>&1
