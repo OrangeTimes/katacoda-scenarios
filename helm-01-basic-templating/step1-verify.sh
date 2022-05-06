879b41d6948f6854b10ad70094e6043f166b0dac
Volodymyr Golub <volodymyr.golub@justanswer.com>
2022-05-06 16:12:51 +0300

    udpated step

--- helm-01-basic-templating/step1-verify.sh
+++ helm-01-basic-templating/step1-verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# pwsh -File ~/.hidden/tests/runTest.ps1 -testLocation '~/.hidden/tests/step01' && echo "done"
-pwsh -c "ls" && echo "done" 
+pwsh -File ~/.hidden/tests/runTest.ps1 -testLocation '~/.hidden/tests/step01' && echo "done"
+
 # echo $? >> /tmp/result.txt 2>&1
 # OUTPUT=$?
 # echo "${OUTPUT}" >> /tmp/result.txt 2>&1
